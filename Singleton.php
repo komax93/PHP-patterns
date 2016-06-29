a16414337c186666ebe32c0d72a0fbb5360ce1c7
Maksym Kolesnykov <mv.kolesnikov@1plus1.tv>
2016-06-29 19:05:21 +0300

    Changed singleton

--- Singleton.php
+++ Singleton.php
@@ -1,2 +1,35 @@
 <?php
 
+class Singleton
+{
+    private $props = array();
+    private static $instance;
+
+    private function __construct(){}
+
+    public static function getInstance()
+    {
+        if(empty(self::$instance))
+        {
+            self::$instance = new Singleton();
+        }
+
+        return self::$instance;
+    }
+
+    public function setProperty($key, $value)
+    {
+        $this->props[$key] = $value;
+    }
+
+    public function getPropery($key)
+    {
+        return $this->props[$key];
+    }
+}
+
+$pref = Singleton::getInstance();
+$pref->setProperty("key", 1);
+
+$pref2 = Singleton::getInstance();
+print $pref2->getPropery(key);
