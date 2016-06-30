c02dfb4decc4b2318dceeede64fe1d2de09a89c7
Maxym Kolesnykov <maxkolesnikov93@gmail.com>
2016-06-30 23:12:07 +0300

    Added AbstractFactory

--- AbstractFactory.php
+++ AbstractFactory.php
@@ -1,1 +1,38 @@
 <?php
+
+abstract class CommsManager
+{
+    abstract function getHeaderText();
+    abstract function getApptEncoder();
+    abstract function getTtdEncoder();
+    abstract function getContactEncoder();
+    abstract function getFooterText();
+}
+
+class BlogsCommsManager extends CommsManager
+{
+    function getHeaderText()
+    {
+        return "BloggsCal верхний колонтитул\n";
+    }
+
+    function getApptEncoder()
+    {
+        return new BloggsTtdEncoder();
+    }
+
+    function getTtdEncoder()
+    {
+        return new BloggsTtdEncoder();
+    }
+
+    function getContactEncoder()
+    {
+        return new BlogsContactEncoder();
+    }
+
+    function getFooterText()
+    {
+        return "BloggsCal нижний колонтитул\n";
+    }
+}
